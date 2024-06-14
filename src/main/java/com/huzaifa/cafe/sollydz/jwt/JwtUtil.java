eba271d68b195c496aff04443f033f8fe273f2ea
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-06-14 23:34:36 +0530

    part 2 dependency added - bug fixed

--- src/main/java/com/huzaifa/cafe/sollydz/jwt/JwtUtil.java
+++ src/main/java/com/huzaifa/cafe/sollydz/jwt/JwtUtil.java
@@ -1,8 +1,20 @@
 package com.huzaifa.cafe.sollydz.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 public class JwtUtil {
+    private String secret = "huzaifa20020705";
 
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    public Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
 }
